a1a6d9c402c43f09004fdef79079396324910358
whuxm <3165684725@qq.com>
2024-02-19 08:43:43 +0800

    新更改

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 
 int main(int, char **)
 {
+    int year;
     std::cout << "Hello, from demo!\n";
     std::cout << "This is the second line!\n";
+    std::cout << "Please enter a year: ";
+    std::cin >> year;
+    std::cout << "The year is now " << year << "!\n";
     return 0; // success
 }
